ee80fa9f15522c3f6ea30cf9f7c41a1e48146173
Tarun Potti <info@technosheens.com>
2021-07-15 11:18:28 +0530

    model fixes

--- src/main/java/com/dealbab/model/HotDeal.java
+++ src/main/java/com/dealbab/model/HotDeal.java
@@ -12,13 +12,8 @@ public class HotDeal {
 private Integer id;
 private String productTitle;
 private String productShortDesc;
-private String productLongDesc;
 private String productModel;
 private String currencyCode;
-private String sku;
-private String rating;
-private String createdDate;
-private String updatedDate;
 private String publishedDate;
 private String basePrice;
 private String discountPrice;
@@ -26,16 +21,15 @@ private String discountPercentage;
 private String quantity;
 private String vaildFrom;
 private String vaildUntil;
-private String createdBy;
-private String updatedBy;
-private String sid;
-private String oid;
-private String storeName;
-private String storeImageUrl;
+private String stroreId;
 private String storeRedirectUrl;
-private List<Variant> variant;
 private List<Category> categories ;
 private List<String> region = null;
+private List<String> size = null;
+private List<String> colour=null;
+private String productImageURl;
+private String rating;
+private String storeName;
 
 public Integer getId() {
 	return id;
@@ -55,12 +49,6 @@ public String getProductShortDesc() {
 public void setProductShortDesc(String productShortDesc) {
 	this.productShortDesc = productShortDesc;
 }
-public String getProductLongDesc() {
-	return productLongDesc;
-}
-public void setProductLongDesc(String productLongDesc) {
-	this.productLongDesc = productLongDesc;
-}
 public String getProductModel() {
 	return productModel;
 }
@@ -72,30 +60,6 @@ public String getCurrencyCode() {
 }
 public void setCurrencyCode(String currencyCode) {
 	this.currencyCode = currencyCode;
-}
-public String getSku() {
-	return sku;
-}
-public void setSku(String sku) {
-	this.sku = sku;
-}
-public String getRating() {
-	return rating;
-}
-public void setRating(String rating) {
-	this.rating = rating;
-}
-public String getCreatedDate() {
-	return createdDate;
-}
-public void setCreatedDate(String createdDate) {
-	this.createdDate = createdDate;
-}
-public String getUpdatedDate() {
-	return updatedDate;
-}
-public void setUpdatedDate(String updatedDate) {
-	this.updatedDate = updatedDate;
 }
 public String getPublishedDate() {
 	return publishedDate;
@@ -139,53 +103,17 @@ public String getVaildUntil() {
 public void setVaildUntil(String vaildUntil) {
 	this.vaildUntil = vaildUntil;
 }
-public String getCreatedBy() {
-	return createdBy;
+public String getStroreId() {
+	return stroreId;
 }
-public void setCreatedBy(String createdBy) {
-	this.createdBy = createdBy;
-}
-public String getUpdatedBy() {
-	return updatedBy;
-}
-public void setUpdatedBy(String updatedBy) {
-	this.updatedBy = updatedBy;
-}
-public String getSid() {
-	return sid;
-}
-public void setSid(String sid) {
-	this.sid = sid;
-}
-public String getOid() {
-	return oid;
-}
-public void setOid(String oid) {
-	this.oid = oid;
-}
-public String getStoreName() {
-	return storeName;
-}
-public void setStoreName(String storeName) {
-	this.storeName = storeName;
-}
-public String getStoreImageUrl() {
-	return storeImageUrl;
-}
-public void setStoreImageUrl(String storeImageUrl) {
-	this.storeImageUrl = storeImageUrl;
+public void setStroreId(String stroreId) {
+	this.stroreId = stroreId;
 }
 public String getStoreRedirectUrl() {
 	return storeRedirectUrl;
 }
 public void setStoreRedirectUrl(String storeRedirectUrl) {
 	this.storeRedirectUrl = storeRedirectUrl;
-}
-public List<Variant> getVariant() {
-	return variant;
-}
-public void setVariant(List<Variant> variant) {
-	this.variant = variant;
 }
 public List<Category> getCategories() {
 	return categories;
@@ -199,18 +127,48 @@ public List<String> getRegion() {
 public void setRegion(List<String> region) {
 	this.region = region;
 }
+public List<String> getSize() {
+	return size;
+}
+public void setSize(List<String> size) {
+	this.size = size;
+}
+public List<String> getColour() {
+	return colour;
+}
+public void setColour(List<String> colour) {
+	this.colour = colour;
+}
+public String getProductImageURl() {
+	return productImageURl;
+}
+public void setProductImageURl(String productImageURl) {
+	this.productImageURl = productImageURl;
+}
+public String getRating() {
+	return rating;
+}
+public void setRating(String rating) {
+	this.rating = rating;
+}
+public String getStoreName() {
+	return storeName;
+}
+public void setStoreName(String storeName) {
+	this.storeName = storeName;
+}
 @Override
 public String toString() {
-	return "Product [id=" + id + ", productTitle=" + productTitle + ", productShortDesc=" + productShortDesc
-			+ ", productLongDesc=" + productLongDesc + ", productModel=" + productModel + ", currencyCode="
-			+ currencyCode + ", sku=" + sku + ", rating=" + rating + ", createdDate=" + createdDate
-			+ ", updatedDate=" + updatedDate + ", publishedDate=" + publishedDate + ", basePrice=" + basePrice
-			+ ", discountPrice=" + discountPrice + ", discountPercentage=" + discountPercentage + ", quantity="
-			+ quantity + ", vaildFrom=" + vaildFrom + ", vaildUntil=" + vaildUntil + ", createdBy=" + createdBy
-			+ ", updatedBy=" + updatedBy + ", sid=" + sid + ", oid=" + oid + ", storeName=" + storeName
-			+ ", storeImageUrl=" + storeImageUrl + ", storeRedirectUrl=" + storeRedirectUrl + ", variant=" + variant
-			+ ", categories=" + categories + ", region=" + region + "]";
-
+	return "HotDeal [id=" + id + ", productTitle=" + productTitle + ", productShortDesc=" + productShortDesc
+			+ ", productModel=" + productModel + ", currencyCode=" + currencyCode + ", publishedDate=" + publishedDate
+			+ ", basePrice=" + basePrice + ", discountPrice=" + discountPrice + ", discountPercentage="
+			+ discountPercentage + ", quantity=" + quantity + ", vaildFrom=" + vaildFrom + ", vaildUntil=" + vaildUntil
+			+ ", stroreId=" + stroreId + ", storeRedirectUrl=" + storeRedirectUrl + ", categories=" + categories
+			+ ", region=" + region + ", size=" + size + ", colour=" + colour + ", productImageURl=" + productImageURl
+			+ ", rating=" + rating + ", storeName=" + storeName + "]";
 }
+
+
+
 
 }
